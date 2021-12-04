a6fa3c3b9c8fc10e1ba0452b8a880d904f880800
ThunderNL <stijntdm@gmail.com>
2021-12-04 12:38:51 +0100

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     led.setBrightness(led.brightness() + 25.5)
 })
+led.setBrightness(0)
 basic.forever(function () {
     if (input.temperature() == -5) {
         basic.showLeds(`
